d4a220cd31a2f2bcb0a913dab43b7ec3ec44aafc
Abdelrahman <abdokhm13@gmail.com>
2025-11-25 00:01:30 +0200

    Add Application User

--- Bulky.DataAccess/Data/AppDbContext.cs
+++ Bulky.DataAccess/Data/AppDbContext.cs
@@ -9,7 +9,7 @@ namespace BulkyBook.DataAccess.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
