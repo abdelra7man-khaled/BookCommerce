bf222b6bc51420a8c936573d668dd4113f21a244
Abdelrahman <abdokhm13@gmail.com>
2025-12-02 01:18:26 +0200

    add show password checkbox to login form

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -86,5 +86,8 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             TempData["success"] = "Category Deleted successfully";
             return RedirectToAction("Index");
         }
+
+
     }
+
 }
