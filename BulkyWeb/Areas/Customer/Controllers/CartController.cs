1c5ac8dbee89afa5cc46299dc653e475e74c326f
Abdelrahman <abdokhm13@gmail.com>
2025-12-02 16:37:25 +0200

    Add increase or decrease product count in Cart and delete this product

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -34,6 +34,65 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        [HttpGet]
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+
+        public IActionResult IncreaseQuantity(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+            if (cart is null)
+            {
+                return NotFound("Cart not found");
+            }
+            cart.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult DecreaseQuantity(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+            if (cart is null)
+            {
+                return NotFound("Cart not found");
+            }
+
+            if (cart.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+            }
+            else
+            {
+                cart.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cart);
+            }
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult DeleteFromCart(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId, includeProperties: "Product");
+            if (cart is null)
+            {
+                return NotFound("Cart not found");
+            }
+
+            string productName = cart.Product?.Title ?? string.Empty;
+
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            TempData["success"] = $" {productName} product successfully deleted from your cart";
+            return RedirectToAction(nameof(Index));
+        }
+
         private decimal GetPriceBasedOnQuantity(ShoppingCart cart)
         {
             if (cart.Count <= 50)
