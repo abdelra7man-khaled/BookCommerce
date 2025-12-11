9c52554c239973b2c5e154dacc5b4f8d605f6fe9
Abdelrahman <abdokhm13@gmail.com>
2025-12-11 02:18:17 +0200

    add SessionId to OrderHeader and add update functionality to OrderHeaderRepo to update orderStatus or paymentStatus

--- Bulky.DataAccess/Repository/OrderHeaderRepository.cs
+++ Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -16,5 +16,33 @@ namespace BulkyBook.DataAccess.Repository
             _context.Update(orderHeaderToUpdate);
         }
 
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var order = _context.OrderHeaders.FirstOrDefault(o => o.Id == id);
+            if (order is not null)
+            {
+                order.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    order.PaymentStatus = paymentStatus;
+                }
+
+            }
+        }
+
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+        {
+            var order = _context.OrderHeaders.FirstOrDefault(o => o.Id == id);
+
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                order!.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                order!.PaymentIntentId = paymentIntentId;
+                order.PaymentDate = DateTime.Now;
+            }
+        }
     }
 }
