ef94b0e9638634e1c0a4b3a240208e563e0980ac
Abdelrahman <abdokhm13@gmail.com>
2025-12-15 22:10:24 +0200

    Add OrderController for order managment using data tables , and add OrderVM

---
+++ Bulky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,8 @@
+﻿namespace BulkyBook.Models.ViewModels
+{
+    public class OrderVM
+    {
+        OrderHeader OrderHeader { get; set; }
+        IEnumerable<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBook.DataAccess.Repository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = StaticDetails.Role_Admin)]
+    public class OrderController(UnitOfWork _unitOfWork) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = orderHeaders });
+        }
+
+
+        #endregion
+    }
+}
