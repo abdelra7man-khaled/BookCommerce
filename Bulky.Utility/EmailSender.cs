deabfdc5aeef0c08e0dd74dbf9889ffda21a5cad
Abdelrahman <abdokhm13@gmail.com>
2025-12-18 03:50:59 +0200

    Implement EmailSender to send emails to users

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration configuration)
+        {
+            SendGridSecret = configuration.GetValue<string>("SendGrid:SecretKey")!;
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var client = new SendGridClient(SendGridSecret);
+
+            var from = new EmailAddress("dev.abdelrahmankhaled@gmail.com", "Bulky Book");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+
+            //return client.SendEmailAsync(message);
             return Task.CompletedTask;
         }
     }
