3a16559ee95f743c6856db7053dcc28b714cb118
Abdelrahman <abdokhm13@gmail.com>
2025-12-22 00:54:41 +0200

    Add User Dropdown menu to the admin and user controller

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
+    public class UserController(AppDbContext _context) : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> users = _context.ApplicationUsers.Include(u => u.Company).ToList();
+            return Json(new { data = users });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { success = true, message = "the company deleted successfully" });
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,26 @@
+﻿var dataTable;
+
+$(document).ready (function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tableData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        "columns": [
+            { data: 'name', "width": "25%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'company.name', "width": "15%" }, 
+            { data: '', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group role="group">
+                                <a href="/Admin/User/Upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
+                            </div>`
+                }
+            }
+        ]
+    });
+}
